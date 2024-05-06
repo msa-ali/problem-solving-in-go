a029b1739a1345794d6e90eb233d877df83fa788
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2024-05-06 09:39:15 +0530

    two city scheduling

--- data-structures/greedy-techniques/004-two-city-scheduling.go
+++ data-structures/greedy-techniques/004-two-city-scheduling.go
@@ -1,1 +1,19 @@
 package greedytechniques
+
+import (
+	"sort"
+)
+
+// https://leetcode.com/problems/two-city-scheduling/description/
+
+func twoCityScheduling(costs [][]int) int {
+	sort.SliceStable(costs, func(i, j int) bool {
+		return (costs[i][0] - costs[i][1]) < (costs[j][0] - costs[j][1])
+	})
+	n := len(costs)
+	totalCost := 0
+	for i := 0; i < n/2; i++ {
+		totalCost += costs[i][0] + costs[n-i-1][1]
+	}
+	return totalCost
+}
--- data-structures/greedy-techniques/backtracking_test.go
+++ data-structures/greedy-techniques/backtracking_test.go
@@ -19,3 +19,7 @@ func TestRescueBoats(t *testing.T) {
 func TestGasStations(t *testing.T) {
 	assert.Equal(t, 3, gasStationJourney([]int{1, 2, 3, 4, 5}, []int{3, 4, 5, 1, 2}))
 }
+
+func TestTwoCityScheduling(t *testing.T) {
+	assert.Equal(t, 350, twoCityScheduling([][]int{{100, 10}, {1000, 10}, {500, 50}, {100, 1}, {50, 1}, {2000, 40}}))
+}
